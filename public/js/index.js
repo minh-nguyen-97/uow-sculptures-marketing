873c7fd6d7aba4c4ea2cd973146f7d1d87a8ad62
AchillesS <qmn1312@gmail.com>
2019-10-26 04:19:05 +1100

    Meet Our Team (Members)

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  $('.card').hover(
-    function() {
-      $(this).addClass('shadow-lg')
-    },
-    function() {
-      $(this).removeClass('shadow-lg')
-    }
-  )
-
   // Smooth scroll for links with hashes
   $('a[href*="#"]')
     // Remove links that don't actually link to anything
@@ -55,5 +46,21 @@ $(document).ready(function() {
   // no auto animation in carousel slide
   $('.carousel').carousel({
     interval: false
+  })
+
+  //
+  $('#membersModal').on('show.bs.modal', function (event) {
+    var card = $(event.relatedTarget) 
+    var imgsrc = card.data('imgsrc')
+    var membername = card.data('membername') 
+    var majordesc = card.data('majordesc');
+    var roledesc = card.data('roledesc');
+
+    var modal = $(this)
+    modal.find('#membersModalImg').attr('src', imgsrc)
+    modal.find('.membersName').text(membername)
+    modal.find('#membersModalMajorDesc').html(majordesc)
+    modal.find('#membersModalRoleDesc').html(roledesc)
+
   })
 })
